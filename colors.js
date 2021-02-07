068483a28b9cd041015bc09abd6a23bf52c55a63
lsck112 <78213140+lsck112@users.noreply.github.com>
2021-02-07 20:10:01 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,23 @@
+function setColor (color){
+  var alist = document.querySelectorAll('a')
+  var i = 0;
+  while (i < alist.length){
+    alist[i].style.color = color;
+    i = i + 1;
+}
+}
+function nightdayHandler(self){
+var target = document.querySelector('body');
+if (self.value === 'night'){
+  target.style.backgroundColor='black';
+  target.style.color='white';
+  self.value = 'day';
+
+  setColor('white');
+  }
+else{target.style.backgroundColor='white';
+target.style.color = 'black';
+self.value = 'night';
+  setColor('black');
+}
+}
